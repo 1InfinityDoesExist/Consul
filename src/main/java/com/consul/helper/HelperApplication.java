d85253e0abddcd1e21ce0def02b21a5eb4c26527
1InfinityDoesExist <patelavinash508@gmail.com>
2021-12-31 23:40:55 +0545

    consul base plate

---
+++ src/main/java/com/consul/helper/HelperApplication.java
@@ -0,0 +1,15 @@
+package com.consul.helper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@SpringBootApplication
+public class HelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelperApplication.class, args);
+
+	}
+
+}
