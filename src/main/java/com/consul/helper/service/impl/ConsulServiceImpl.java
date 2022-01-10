1eb5b6ff8733e797d335bec0b227c657ec9ffdb6
1InfinityDoesExist <patelavinash508@gmail.com>
2022-01-10 22:03:07 +0545

    consul properties to consul apis

--- src/main/java/com/consul/helper/service/impl/ConsulServiceImpl.java
+++ src/main/java/com/consul/helper/service/impl/ConsulServiceImpl.java
@@ -1,12 +1,21 @@
 package com.consul.helper.service.impl;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Paths;
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Stream;
 
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import com.consul.helper.model.ApplicationPropertiesConsul;
 import com.consul.helper.model.ConsulPropertyCreateRequest;
 import com.consul.helper.model.ConsulPropertyDeleteRequest;
 import com.consul.helper.service.ConsulService;
@@ -47,41 +56,82 @@ public class ConsulServiceImpl implements ConsulService {
 	@Override
 	public Mono<String[]> getAllKeys(String consulURL, String projectPath) {
 		log.info("-----Retrieve all the keys-----");
-		return webClient.get()
-				.uri("http://" + consulURL + "/v1/kv/config/" + projectPath + "/?keys")
-				.retrieve()
+		return webClient.get().uri("http://" + consulURL + "/v1/kv/config/" + projectPath + "/?keys").retrieve()
 				.bodyToMono(String[].class)
 				.map(res -> {
 					return Stream.of(res)
 							.map(prop -> prop.replaceAll("config/" + projectPath + "/", ""))
 							.filter(k -> k.length() > 0)
 							.toArray(String[]::new);
-				})
-				.log();
+				}).log();
 	}
 
-	
 	/**
-	 * via payload string will contain "" as a result the value in consul will also contain ""
+	 * via payload string will contain "" as a result the value in consul will also
+	 * contain ""
 	 * 
 	 * properties will be read from application.properties..so no issue.
 	 */
 	@Override
 	public Mono<String> addKeyValuePairInConsulUsingPUT(ConsulPropertyCreateRequest request) {
 		log.info("-----About to add /update key value in consul.-----");
-		request.getProperties().entrySet().stream()
-		.forEach(prop -> {
+		request.getProperties().entrySet().stream().forEach(prop -> {
+			System.out.println(prop.getValue());
 			webClient.put()
-				.uri("http://" + request.getConsulUrl() + "/v1/kv/config/" + request.getProjectPath() + "/"
-								+ prop.getKey())
-				.body(Mono.just(prop.getValue()), Object.class)
-				.accept(MediaType.APPLICATION_JSON)
-				.retrieve()
-				.bodyToMono(Boolean.class)
-				.timeout(Duration.ofMillis(5000))
-				.subscribe();
+					.uri("http://" + request.getConsulUrl() + "/v1/kv/config/" + request.getProjectPath() + "/"
+							+ prop.getKey())
+					.body(Mono.just(prop.getValue()), Object.class)
+					.accept(MediaType.APPLICATION_JSON)
+					.retrieve()
+					.bodyToMono(Boolean.class)
+					.timeout(Duration.ofMillis(5000))
+					.subscribe();
 		});
 		return Mono.just("Success");
 	}
 
+	/**
+	 * Adding all the application.properties key-value to consul.
+	 * 
+	 * @throws IOException
+	 */
+	@Override
+	public Mono<String> appPropertiesToConsul(ApplicationPropertiesConsul request) throws IOException {
+		log.info("----Adding all the application.properties to the consul.");
+		Map<String, Object> properties = new HashMap<>();
+		String projectPath = Paths.get(request.getProjectPath(), "src", "main", "resources", request.getPropFile())
+				.toString();
+		File file = new File(projectPath);
+		if (file.exists()) {
+			log.info("---File does Exist");
+			BufferedReader bufferReader = null;
+			try {
+				bufferReader = new BufferedReader(new FileReader(projectPath));
+				String line;
+				while ((line = bufferReader.readLine()) != null) {
+					if (!line.startsWith("#") && !ObjectUtils.isEmpty(line)) {
+						String[] split = line.split("=");
+						if (split[0].equalsIgnoreCase("server.port")) {
+							continue;
+						}
+						properties.put(split[0], split[1]);
+					}
+				}
+				addKeyValuePairInConsulUsingPUT(ConsulPropertyCreateRequest.builder()
+						.consulUrl(request.getConsulURL())
+						.projectPath(request.getConsulProjectPath())
+						.properties(properties)
+						.build())
+				.subscribe(res -> log.info(res));
+				bufferReader.close();
+			} catch (Exception ex) {
+
+			} finally {
+				bufferReader.close();
+			}
+		} else {
+			log.info("----Fiel does not exist.");
+		}
+		return Mono.just("Success");
+	}
 }
